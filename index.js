6363a013630afd13fce1e5045ed31cf212ff121c
Mohamed Ali <maxali.abdi@gmail.com>
2016-08-16 16:42:31 +0200

    No source map

--- index.js
+++ index.js
@@ -27,19 +27,7 @@ module.exports = function(source, inputSourceMap) {
             }
         };
     
-    // just an indicator to generate source maps, the output result.map will be modified anyway
-    // tell UglifyJS2 not to emit a name by just setting outSourceMap to true
-    opts.outSourceMap = true;
-    opts.fromString = true;
-
     var result = UglifyJS.minify(source, opts);
 
-    var sourceFilename = loaderUtils.getRemainingRequest(this);
-    var current = loaderUtils.getCurrentRequest(this);
-    var sourceMap = JSON.parse(result.map);
-    sourceMap.sources = [sourceFilename];
-    sourceMap.file = current;
-    sourceMap.sourcesContent = [source];
-
-    callback(null, result.code, sourceMap);
+    callback(null, result.code, null);
 };
